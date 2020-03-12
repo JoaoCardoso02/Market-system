14edee18782bc57baaab54b7916c220e51cb7cac
João Lucas <42484336+JoaoCardoso02@users.noreply.github.com>
2020-03-12 09:42:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".container").on("click", function(){
+        $(".menuVertical").toggle("enter");
+    });
+});
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
